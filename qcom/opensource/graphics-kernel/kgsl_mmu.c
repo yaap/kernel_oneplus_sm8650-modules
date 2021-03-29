f7cf2f4cff9d19a7e44417d089385446694533f9
Sultan Alsawaf <sultan@kerneltoast.com>
2021-03-28 18:36:18 -0700

    Revert "msm: kgsl: Add per process GPU memory usage trace"
    This reverts commit d6d18f1f9edbbda8f76905b6bd43711d30481226.
    
    This is debug cruft. Remove it completely since CAF's 4.14 branch
    doesn't have it.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- qcom/opensource/graphics-kernel/kgsl_mmu.c
+++ qcom/opensource/graphics-kernel/kgsl_mmu.c
@@ -192,22 +192,6 @@ err:
 	return ret;
 }
 
-#ifdef CONFIG_TRACE_GPU_MEM
-static void kgsl_mmu_trace_gpu_mem_pagetable(struct kgsl_pagetable *pagetable)
-{
-	if (pagetable->name == KGSL_MMU_GLOBAL_PT ||
-			pagetable->name == KGSL_MMU_SECURE_PT)
-		return;
-
-	trace_gpu_mem_total(0, pagetable->name,
-			(u64)atomic_long_read(&pagetable->stats.mapped));
-}
-#else
-static void kgsl_mmu_trace_gpu_mem_pagetable(struct kgsl_pagetable *pagetable)
-{
-}
-#endif
-
 void
 kgsl_mmu_detach_pagetable(struct kgsl_pagetable *pagetable)
 {
@@ -329,7 +313,6 @@ kgsl_mmu_map(struct kgsl_pagetable *pagetable,
 				struct kgsl_memdesc *memdesc)
 {
 	int size;
-	struct kgsl_device *device = KGSL_MMU_DEVICE(pagetable->mmu);
 
 	if (!memdesc->gpuaddr)
 		return -EINVAL;
@@ -353,12 +336,6 @@ kgsl_mmu_map(struct kgsl_pagetable *pagetable,
 		atomic_inc(&pagetable->stats.entries);
 		KGSL_STATS_ADD(size, &pagetable->stats.mapped,
 				&pagetable->stats.max_mapped);
-		kgsl_mmu_trace_gpu_mem_pagetable(pagetable);
-
-		if (!kgsl_memdesc_is_global(memdesc)
-				&& !(memdesc->flags & KGSL_MEMFLAGS_USERMEM_ION)) {
-			kgsl_trace_gpu_mem_total(device, size);
-		}
 
 		memdesc->priv |= KGSL_MEMDESC_MAPPED;
 	}
@@ -440,7 +417,6 @@ kgsl_mmu_unmap(struct kgsl_pagetable *pagetable,
 		struct kgsl_memdesc *memdesc)
 {
 	int ret = 0;
-	struct kgsl_device *device = KGSL_MMU_DEVICE(pagetable->mmu);
 
 	if (memdesc->size == 0)
 		return -EINVAL;
@@ -463,13 +439,9 @@ kgsl_mmu_unmap(struct kgsl_pagetable *pagetable,
 
 		atomic_dec(&pagetable->stats.entries);
 		atomic_long_sub(size, &pagetable->stats.mapped);
-		kgsl_mmu_trace_gpu_mem_pagetable(pagetable);
 
-		if (!kgsl_memdesc_is_global(memdesc)) {
+		if (!kgsl_memdesc_is_global(memdesc))
 			memdesc->priv &= ~KGSL_MEMDESC_MAPPED;
-			if (!(memdesc->flags & KGSL_MEMFLAGS_USERMEM_ION))
-				kgsl_trace_gpu_mem_total(device, -(size));
-		}
 	}
 
 	return ret;
