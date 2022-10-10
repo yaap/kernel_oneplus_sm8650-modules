d6435f9240cb34e31da29628ccfc4002f583080e
Devendar Gali <quic_dgali@quicinc.com>
2022-10-10 14:17:09 +0530

    NFC: driver: Add a local flag to enable/disable compilation
    Modified code to add a local flag to enable or disable
    module compilation based on system-wide global flags
    for different targets.
    
    Change-Id: I5fbacd7306f355a3f5983dae99e17dd699ace436

--- Android.mk
+++ Android.mk
@@ -12,4 +12,18 @@ LOCAL_SRC_FILES   := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
 
 DLKM_DIR := $(TOP)/device/qcom/common/dlkm
 
-include $(DLKM_DIR)/Build_external_kernelmodule.mk
+NFC_DLKM_ENABLED := false
+
+########## Check and set local DLKM flag based on system-wide global flags ##########
+ifeq ($(TARGET_KERNEL_DLKM_DISABLE), true)
+  ifeq ($(TARGET_KERNEL_DLKM_NFC_OVERRIDE), true)
+    NFC_DLKM_ENABLED := true
+  endif
+else
+  NFC_DLKM_ENABLED := true
+endif
+
+########## Build kernel module based on local DLKM flag status ##########
+ifeq ($(NFC_DLKM_ENABLED), true)
+  include $(DLKM_DIR)/Build_external_kernelmodule.mk
+endif
