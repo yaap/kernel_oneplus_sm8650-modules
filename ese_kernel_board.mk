6212f92e9451edc6077d7fa2985737670c0dce6e
PRANAY BHARGAV BHAVARAJU <quic_pbhavara@quicinc.com>
2022-10-14 16:34:41 +0530

    Driver: eSE: Add Rules to compile eSE GPIO driver for pineapple target
    Modified the ese_kernel_board.mk to enable code compilation for pineapple target
    Also added the dlkm override logic
    
    Change-Id: I00fb1455fd4ad9b95264a716373fd52743126452

--- ese_kernel_board.mk
+++ ese_kernel_board.mk
@@ -1,5 +1,18 @@
-#Kalama specific build rules
-ifeq ($(TARGET_BOARD_PLATFORM),kalama)
-TARGET_USES_ST_ESE := true
-BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/stm_st54se_gpio.ko
+# Check and set local DLKM flag based on system-wide global flags
+ESE_DLKM_ENABLED := false
+
+ifeq ($(TARGET_KERNEL_DLKM_DISABLE), true)
+  ifeq ($(TARGET_KERNEL_DLKM_ESE_OVERRIDE), true)
+    ESE_DLKM_ENABLED := true
+  endif
+else
+  ESE_DLKM_ENABLED := true
+endif
+
+# pineapple specific build rules
+ifeq ($(TARGET_BOARD_PLATFORM),pineapple)
+  ifeq ($(ESE_DLKM_ENABLED), true)
+    TARGET_USES_ST_ESE :=true
+    BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/stm_st54se_gpio.ko
+  endif
 endif
