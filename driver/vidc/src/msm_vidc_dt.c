0e56253be6d62ab05280999311dcf85f683bd3b3
Govindaraj Rajagopal <quic_grajagop@quicinc.com>
2022-10-19 11:45:44 +0530

    video: driver: remove unused files
    msm_vidc_dt functionalities were distributed to
    other files and most of the dt parsing code was
    removed already. So added change to cleanup
    unused files.
    
    Change-Id: I3362c6cfa3f493b965167b9aeb0f5cf3ac8c2711
    Signed-off-by: Govindaraj Rajagopal <quic_grajagop@quicinc.com>

--- driver/vidc/src/msm_vidc_dt.c
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-only
-/*
- * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- */
-
-#include <linux/iommu.h>
-#include <linux/dma-iommu.h>
-#include <linux/of.h>
-#include <linux/sort.h>
