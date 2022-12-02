5679261df394ceb311fb0b93f417f43e79e79942
Erin Yan <quic_xinyey@quicinc.com>
2022-12-02 18:20:25 +0800

    asoc: msm-cdc-pinctrl: fix compile error
    The modifier - static - request the function to be used within the file.
    Remove static modifier to fix the build break.
    
    Change-Id: I46da9dbeb7c3945f8a1bbfa55998e75b5cbdc70b
    Signed-off-by: Erin Yan <quic_xinyey@quicinc.com>

--- include/asoc/msm-cdc-pinctrl.h
+++ include/asoc/msm-cdc-pinctrl.h
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /* Copyright (c) 2016-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef __MFD_CDC_PINCTRL_H_
@@ -47,7 +48,7 @@ int msm_cdc_pinctrl_get_state(struct device_node *np)
 {
 	return true;
 }
-static int msm_cdc_pinctrl_set_wakeup_capable(struct device_node *np,
+int msm_cdc_pinctrl_set_wakeup_capable(struct device_node *np,
 					      bool enable)
 {
 	return 0;
