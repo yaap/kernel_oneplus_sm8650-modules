fb51f095af9c7f183a7edf63c41e6eaf0f6b44a2
pramod kotreshappa <quic_kpramod@quicinc.com>
2023-03-20 14:14:09 -0700

    BTFM techpack make file changes
    Change-Id: I9521acfbcde5ab610ea529c0a0f0ac589bd71a77

--- bt_kernel_vendor_board.mk
+++ bt_kernel_vendor_board.mk
@@ -3,17 +3,18 @@ ifneq ($(TARGET_BOARD_AUTO),true)
 ifeq ($(TARGET_USES_QMAA),true)
   ifeq ($(TARGET_USES_QMAA_OVERRIDE_BLUETOOTH), true)
      ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
-         BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/btpower.ko\
+           BT_KERNEL_DRIVER := $(KERNEL_MODULES_OUT)/btpower.ko\
              $(KERNEL_MODULES_OUT)/bt_fm_slim.ko \
-	     $(KERNEL_MODULES_OUT)/radio-i2c-rtc6226-qca.ko
+             $(KERNEL_MODULES_OUT)/radio-i2c-rtc6226-qca.ko
+           BOARD_VENDOR_KERNEL_MODULES += $(BT_KERNEL_DRIVER)
      endif
   endif
 else
   ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
-     BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/btpower.ko\
+     BT_KERNEL_DRIVER := $(KERNEL_MODULES_OUT)/btpower.ko\
             $(KERNEL_MODULES_OUT)/bt_fm_slim.ko \
-	    $(KERNEL_MODULES_OUT)/radio-i2c-rtc6226-qca.ko
-
+            $(KERNEL_MODULES_OUT)/radio-i2c-rtc6226-qca.ko
+     BOARD_VENDOR_KERNEL_MODULES += $(BT_KERNEL_DRIVER)
   endif
 endif
 endif
