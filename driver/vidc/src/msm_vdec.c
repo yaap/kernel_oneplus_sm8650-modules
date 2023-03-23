c3f3af1e0e3bfc4d4a57eca044225bd1ea8b8df9
Govindaraj Rajagopal <quic_grajagop@quicinc.com>
2023-03-23 13:52:27 +0530

    video: driver: enable memory tracking in stats handler
    track internal memory usage periodically and print
    memory usage stats using stats handler.
    
    Change-Id: Idd16647dbe4aab3fc617a8f7fc0d3c81f459bb5d
    Signed-off-by: Govindaraj Rajagopal <quic_grajagop@quicinc.com>

--- driver/vidc/src/msm_vdec.c
+++ driver/vidc/src/msm_vdec.c
@@ -2156,6 +2156,9 @@ int msm_vdec_start_cmd(struct msm_vidc_inst *inst)
 	/* print final buffer counts & size details */
 	msm_vidc_print_buffer_info(inst);
 
+	/* print internal buffer memory usage stats */
+	msm_vidc_print_memory_stats(inst);
+
 	rc = msm_vidc_process_resume(inst);
 	if (rc)
 		return rc;
