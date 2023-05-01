9c554efeb164d027caaded261f890597e0f87cb3
Anthony Adamo <quic_aadamo@quicinc.com>
2023-05-01 11:33:57 -0700

    ubwcp: Add DLKM flag to guard compilation of ubwcp.ko
    qmaa mode exposes dependencies on ubwcp.ko compilation
    need to guard compilation of ubqcp.ko by dlkm flag
    
    Change-Id: I2e171f2ae837b8ca7aa3e1e0d62731d4d3dde750
    Signed-off-by: Anthony Adamo <quic_aadamo@quicinc.com>

--- ubwcp/Android.mk
+++ ubwcp/Android.mk
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
+ifneq ($(TARGET_KERNEL_DLKM_DISABLE), true)
 LOCAL_PATH := $(call my-dir)
 LOCAL_MODULE_DDK_BUILD := true
 
@@ -11,3 +12,4 @@ LOCAL_EXPORT_KO_INCLUDE_DIRS    += $(LOCAL_PATH)/include/kernel
 LOCAL_MODULE      := ubwcp.ko
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
+endif
