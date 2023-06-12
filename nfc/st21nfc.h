62a634bd49827bd7be75ff10d622e81dcadb84a7
Devendar Gali <quic_dgali@quicinc.com>
2023-06-12 18:38:39 +0530

    st nfc: driver: fix vedor checker errors
    Added fix to Vendor checker compilation errors.
    
    Change-Id: Ia120efd89eb9b84dd0588dbeada299b6cfcf28f3
    Signed-off-by: Devendar Gali <quic_dgali@quicinc.com>

--- nfc/st21nfc.h
+++ nfc/st21nfc.h
@@ -1,36 +1,15 @@
-// SPDX-License-Identifier: GPL-2.0-only
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * NFC Controller Driver
  * Copyright (C) 2020 ST Microelectronics S.A.
  * Copyright (C) 2010 Stollmann E+V GmbH
  * Copyright (C) 2010 Trusted Logic S.A.
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #define ST21NFC_MAGIC 0xEA
 
 #define ST21NFC_NAME "st21nfc"
-/*
- * ST21NFC power control via ioctl
- * ST21NFC_GET_WAKEUP :  poll gpio-level for Wakeup pin
- */
-#define ST21NFC_GET_WAKEUP _IO(ST21NFC_MAGIC, 0x01)
-#define ST21NFC_PULSE_RESET _IO(ST21NFC_MAGIC, 0x02)
-#define ST21NFC_SET_POLARITY_RISING _IO(ST21NFC_MAGIC, 0x03)
-#define ST21NFC_SET_POLARITY_HIGH _IO(ST21NFC_MAGIC, 0x05)
-#define ST21NFC_GET_POLARITY _IO(ST21NFC_MAGIC, 0x07)
-#define ST21NFC_RECOVERY _IO(ST21NFC_MAGIC, 0x08)
-#define ST21NFC_USE_ESE _IOW(ST21NFC_MAGIC, 0x09, unsigned int)
-
-#define NFC_SECURE_ZONE _IO(ST21NFC_MAGIC, 0x0B)
-
-// Keep compatibility with older user applications.
-#define ST21NFC_LEGACY_GET_WAKEUP _IOR(ST21NFC_MAGIC, 0x01, unsigned int)
-#define ST21NFC_LEGACY_PULSE_RESET _IOR(ST21NFC_MAGIC, 0x02, unsigned int)
-#define ST21NFC_LEGACY_SET_POLARITY_RISING                                     \
-	_IOR(ST21NFC_MAGIC, 0x03, unsigned int)
-#define ST21NFC_LEGACY_SET_POLARITY_HIGH _IOR(ST21NFC_MAGIC, 0x05, unsigned int)
-#define ST21NFC_LEGACY_GET_POLARITY _IOR(ST21NFC_MAGIC, 0x07, unsigned int)
-#define ST21NFC_LEGACY_RECOVERY _IOR(ST21NFC_MAGIC, 0x08, unsigned int)
 
 #define ST54SPI_CB_RESET_END 0
 #define ST54SPI_CB_RESET_START 1
