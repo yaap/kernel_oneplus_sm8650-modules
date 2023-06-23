b5912cd2ea1b447ab351ba0bdb745baacc77a614
Dikshita Agarwal <quic_dikshita@quicinc.com>
2023-06-23 11:45:19 +0530

    video: driver: remove unused APIs and other fixes/cleanup
    - Remove unused APIs:
        buf_cleanup
        _lock_check
        msm_vdec_update_max_map_output_count
    - Rename msm_vidc_print_insts_info to msm_vidc_print_running_instances_info
      for better understanding.
    
    Change-Id: I1ae190cbf02a52554bfe286c5e336fad37d7419e
    Signed-off-by: Dikshita Agarwal <quic_dikshita@quicinc.com>

--- driver/vidc/inc/msm_vidc_vb2.h
+++ driver/vidc/inc/msm_vidc_vb2.h
@@ -43,7 +43,6 @@ int msm_vidc_stop_streaming(struct msm_vidc_inst *inst, struct vb2_queue *q);
 int msm_vb2_start_streaming(struct vb2_queue *q, unsigned int count);
 void msm_vb2_stop_streaming(struct vb2_queue *q);
 void msm_vb2_buf_queue(struct vb2_buffer *vb2);
-void msm_vb2_buf_cleanup(struct vb2_buffer *vb);
 int msm_vb2_buf_out_validate(struct vb2_buffer *vb);
 void msm_vb2_request_complete(struct vb2_buffer *vb);
 #endif // _MSM_VIDC_VB2_H_
