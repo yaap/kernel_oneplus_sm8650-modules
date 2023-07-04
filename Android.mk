c4e8550d3bceaebbe4a94def18efe6e38d9d80e7
Surya Teja Kudiri <quic_skudiri@quicinc.com>
2023-07-04 12:40:12 +0530

    touch: trinket: enable touch driver
    Enable synaptics_tcm touch driver for trinket target.
    
    Change-Id: I7c6233b4f345c52a01ebebb92da2cb7d546ef24f
    Signed-off-by: Surya Teja Kudiri <quic_skudiri@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -250,6 +250,19 @@ else ifeq ($(TARGET_BOARD_PLATFORM), bengal)
        include $(DLKM_DIR)/Build_external_kernelmodule.mk
        ###########################################################
 
+else ifeq ($(TARGET_BOARD_PLATFORM), trinket)
+
+       ###########################################################
+       include $(CLEAR_VARS)
+       LOCAL_SRC_FILES   := $(wildcard $(LOCAL_PATH)/**/*) $(wildcard $(LOCAL_PATH)/*)
+       LOCAL_MODULE              := synaptics_tcm_ts.ko
+       LOCAL_MODULE_KBUILD_NAME  := synaptics_tcm_ts.ko
+       LOCAL_MODULE_TAGS         := optional
+       #LOCAL_MODULE_DEBUG_ENABLE := true
+       LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+       include $(DLKM_DIR)/Build_external_kernelmodule.mk
+       ###########################################################
+
 else
 
        ###########################################################
