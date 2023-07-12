9f21e8cfbc3b1b5368dd98293b7804f64427d754
Amit Kumar Prasad <quic_amitpras@quicinc.com>
2023-07-12 15:18:40 +0530

    niobe: Disable bt-kernel ko compilation
    This will disable bt-kernel ko compilation based on the board property.
    
    Change-Id: Ia2a4a55ee4b69b447db81882ea93c92380b771da
    Signed-off-by: Amit Kumar Prasad <quic_amitpras@quicinc.com>

--- bt_kernel_vendor_board.mk
+++ bt_kernel_vendor_board.mk
@@ -11,10 +11,12 @@ ifeq ($(TARGET_USES_QMAA),true)
   endif
 else
   ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
-     BT_KERNEL_DRIVER := $(KERNEL_MODULES_OUT)/btpower.ko\
+     ifneq ($(TARGET_BOARD_PLATFORM), niobe)
+        BT_KERNEL_DRIVER := $(KERNEL_MODULES_OUT)/btpower.ko\
             $(KERNEL_MODULES_OUT)/bt_fm_slim.ko \
             $(KERNEL_MODULES_OUT)/radio-i2c-rtc6226-qca.ko
-     BOARD_VENDOR_KERNEL_MODULES += $(BT_KERNEL_DRIVER)
+        BOARD_VENDOR_KERNEL_MODULES += $(BT_KERNEL_DRIVER)
+     endif
   endif
 endif
 endif
