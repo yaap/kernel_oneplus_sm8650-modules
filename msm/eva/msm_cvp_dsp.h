73269ff609d24d1d8cd2d14006d98f2308af8d6d
George Shen <quic_sqiao@quicinc.com>
2023-07-18 12:14:16 -0700

    msm: eva: Remove unused dsp code
    Remove unnecesary warning.
    
    Change-Id: I6517a4eb22a9cbbb692d7e09a5b0edde54fc888d
    Signed-off-by: George Shen <quic_sqiao@quicinc.com>

--- msm/eva/msm_cvp_dsp.h
+++ msm/eva/msm_cvp_dsp.h
@@ -268,22 +268,6 @@ int cvp_dsp_resume(void);
 int cvp_dsp_shutdown(void);
 
 /*
- * API to register iova buffer address with CDSP
- *
- * @session_id:     cvp session id
- * @buff_fd:        buffer fd
- * @buff_fd_size:   total size of fd in bytes
- * @buff_size:      size in bytes of cvp buffer
- * @buff_offset:    buffer offset
- * @buff_index:     buffer index
- * @iova_buff_addr: IOVA buffer address
- */
-int cvp_dsp_register_buffer(uint32_t session_id, uint32_t buff_fd,
-			uint32_t buff_fd_size, uint32_t buff_size,
-			uint32_t buff_offset, uint32_t buff_index,
-			uint32_t buff_fd_iova);
-
-/*
  * API to de-register iova buffer address from CDSP
  *
  * @session_id:     cvp session id
