936925471de7c957aaf4e0c4a91a9a5ba6615a42
George Shen <quic_sqiao@quicinc.com>
2023-07-28 19:28:10 -0700

    msm: eva: Enhanced check to avoid kernel panic
    Avoid delete non-existing session and power on check before
    register write.
    
    Change-Id: I0b7d5045d68fd18e5a9a041d3ad3e37f4dac16ad
    Signed-off-by: George Shen <quic_sqiao@quicinc.com>

--- msm/eva/msm_cvp_core.c
+++ msm/eva/msm_cvp_core.c
@@ -286,7 +286,7 @@ check_again:
 	}
 }
 
-static void msm_cvp_cleanup_instance(struct msm_cvp_inst *inst)
+static int msm_cvp_cleanup_instance(struct msm_cvp_inst *inst)
 {
 	bool empty;
 	int rc, max_retries;
@@ -297,7 +297,7 @@ static void msm_cvp_cleanup_instance(struct msm_cvp_inst *inst)
 
 	if (!inst) {
 		dprintk(CVP_ERR, "%s: invalid params\n", __func__);
-		return;
+		return -EINVAL;
 	}
 
 	sqf = &inst->session_queue_fence;
@@ -357,7 +357,7 @@ stop_session:
 	if (!tmp) {
 		dprintk(CVP_ERR, "%s has a invalid session %llx\n",
 			__func__, inst);
-		return;
+		return -EINVAL;
 	}
 	if (!empty) {
 		/* STOP SESSION to avoid SMMU fault after releasing ARP */
@@ -395,6 +395,7 @@ release_arp:
 		hdev = inst->core->device;
 		call_hfi_op(hdev, pm_qos_update, hdev->hfi_device_data);
 	}
+	return 0;
 }
 
 int msm_cvp_destroy(struct msm_cvp_inst *inst)
@@ -499,7 +500,9 @@ int msm_cvp_close(void *instance)
 	}
 
 	if (inst->session_type != MSM_CVP_BOOT) {
-		msm_cvp_cleanup_instance(inst);
+		rc = msm_cvp_cleanup_instance(inst);
+		if (rc)
+			return -EINVAL;
 		msm_cvp_session_deinit(inst);
 	}
 
