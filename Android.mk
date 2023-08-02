2fd10c20bd0d85124a2a15549b343a3f09615b59
Sri Krishna Chaitanya Valluri <quic_svalluri@quicinc.com>
2023-08-01 17:42:58 -0700

    fingerprint: Add DLKM rule for fingeprint module
    Add new FINGERPRINT_DLKM_ENABLED flag to enable/disable
    compilation of Fingeprint module from target level makefile.
    This is particularly needed for Dev SP (LA.VENDOR.0.15.3) BU.
    
    Change-Id: I91832510692c8bb5a5956090f52a83046e8079aa
    Signed-off-by: Sri Krishna Chaitanya Valluri <quic_svalluri@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,12 @@
 # Android makefile for Fingerprint kernel modules
+FINGERPRINT_DLKM_ENABLED := true
+ifeq ($(TARGET_KERNEL_DLKM_DISABLE), true)
+       ifeq ($(TARGET_KERNEL_DLKM_FINGERPRINT_OVERRIDE), false)
+             FINGERPRINT_DLKM_ENABLED := false;
+       endif
+endif
+
+ifeq ($(FINGERPRINT_DLKM_ENABLED), true)
 
 ifeq ($(call is-board-platform-in-list,pineapple), true)
 ifneq (,$(filter arm aarch64 arm64, $(TARGET_ARCH)))
@@ -15,5 +23,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
 
+endif
 endif
 endif
