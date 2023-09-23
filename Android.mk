cac00bd15ac7b8c9e5ba79f12165c709d696b29d
Ananth Sadvi Bogavalli <quic_abogaval@quicinc.com>
2023-09-23 21:07:49 +0530

    securemsm-kernel: Update hardcoded paths
    Updating the hardcoded path to store into a variable to support
    multiple vendor components in parallel for build compilations.
    
    Tests:
    Compilation validation and device is booting up successfully.
    
    Change-Id: I6a058e538a5050066de0737b834ec182c1283fc3
    Signed-off-by: Ananth Sadvi Bogavalli <quic_abogaval@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -13,9 +13,13 @@ ifeq ($(TARGET_KERNEL_DLKM_DISABLE), true)
 endif
 
 LOCAL_PATH := $(call my-dir)
-DLKM_DIR := $(TOP)/device/qcom/common/dlkm
 
-SEC_KERNEL_DIR := $(TOP)/vendor/qcom/opensource/securemsm-kernel
+VENDOR_OPENSOURCE_DIR ?= vendor/qcom/opensource
+VENDOR_COMMON_DIR ?= device/qcom/common
+
+DLKM_DIR := $(TOP)/$(VENDOR_COMMON_DIR)/dlkm
+
+SEC_KERNEL_DIR := $(TOP)/$(VENDOR_OPENSOURCE_DIR)/securemsm-kernel
 
 LOCAL_EXPORT_KO_INCLUDE_DIRS := $(LOCAL_PATH)/include/ \
                                 $(LOCAL_PATH)/include/uapi
