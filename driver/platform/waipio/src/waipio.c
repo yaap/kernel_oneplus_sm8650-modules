6a72e516fbdb56fc16a16eaac0911ef464b74e61
Ankush Mitra <quic_ankumitr@quicinc.com>
2023-10-20 11:21:15 +0530

    video: driver: remove redundant entry for HOST_MAX_COUNT
    Remove redundant entry for HOST_MAX_COUNT.
    
    Change-Id: If9103e1ae6f435dc0b55cc54b8f26dab6b47bcb7
    Signed-off-by: Ankush Mitra <quic_ankumitr@quicinc.com>

--- driver/platform/waipio/src/waipio.c
+++ driver/platform/waipio/src/waipio.c
@@ -1559,17 +1559,7 @@ static struct msm_platform_inst_cap_dependency instance_cap_dependency_data_waip
 		msm_vidc_adjust_input_buf_host_max_count,
 		msm_vidc_set_u32},
 
-	{INPUT_BUF_HOST_MAX_COUNT, ENC, H264 | HEVC,
-		{0},
-		msm_vidc_adjust_input_buf_host_max_count,
-		msm_vidc_set_u32},
-
 	{OUTPUT_BUF_HOST_MAX_COUNT, ENC | DEC, CODECS_ALL,
-		{0},
-		msm_vidc_adjust_output_buf_host_max_count,
-		msm_vidc_set_u32},
-
-	{OUTPUT_BUF_HOST_MAX_COUNT, ENC, H264 | HEVC,
 		{0},
 		msm_vidc_adjust_output_buf_host_max_count,
 		msm_vidc_set_u32},
