a2ca369213446717cb7adf4c8e9d01ca2a97c499
Kamal Agrawal <quic_kamaagra@quicinc.com>
2023-12-24 12:03:25 +0530

    kgsl: Discard force bus/clk/rail sysfs requests for gmu targets
    For GMU targets, GMU controls the bus/clk/rail voting. Currently,
    forcing clock and rail state doesn't have any impact for GMU
    targets. But, force bus on request can lead to invalid bus voting.
    Since these sysfs are not functional for GMU targets, discard the
    force bus/clk/rail sysfs requests.
    
    Change-Id: Icd5e0ab6f8f21bd6a339d75b3c73365195e9ac55
    Signed-off-by: Kamal Agrawal <quic_kamaagra@quicinc.com>
    (cherry picked from commit ff7d04abee4cd989b828c89045d11d79286547bf)

--- kgsl_pwrctrl.c
+++ kgsl_pwrctrl.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2010-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/clk/qcom.h>
@@ -700,6 +700,9 @@ static ssize_t __force_on_store(struct device *dev,
 	unsigned int val = 0;
 	struct kgsl_device *device = dev_get_drvdata(dev);
 	int ret;
+
+	if (gmu_core_gpmu_isenabled(device))
+		return -EOPNOTSUPP;
 
 	ret = kstrtou32(buf, 0, &val);
 	if (ret)
