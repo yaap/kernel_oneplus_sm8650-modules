702fbcc6f1531a3ec1a2dcf986cae1fe4600f9d6
Gopireddy Arunteja Reddy <quic_garuntej@quicinc.com>
2024-01-12 11:48:55 +0530

    msm: eva: memset the cvp_dsp_cmd_msg structure to 0
    memset cvp_dsp_cmd_msg structure to 0 after dsp thread wakes
    up by rpmsg callback. This avoids a corner case where hlos
    driver sends the previous cmd results even if current cmd
    fails.
    
    Change-Id: I672c4a2a1f0f0cb231fe9eebfc5f2ed86372cbe1
    Signed-off-by: Aniruddh Sharma <quic_anirshar@quicinc.com>
    Signed-off-by: Gopireddy Arunteja Reddy <quic_garuntej@quicinc.com>

--- msm/eva/msm_cvp_dsp.c
+++ msm/eva/msm_cvp_dsp.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 #include <linux/module.h>
 #include <linux/rpmsg.h>
@@ -2046,6 +2046,8 @@ wait_dsp:
 		goto wait_dsp;
 	}
 
+	/* Set the cmd to 0 to avoid sending previous session values in case the command fails*/
+	memset(&cmd, 0, sizeof(struct cvp_dsp_cmd_msg));
 	cmd.type = me->pending_dsp2cpu_cmd.type;
 
 	if (rc == -ERESTARTSYS) {
