bc7045745505a462cd8f4a15c15e8bf0a27b7b36
Vangala, Amarnath <quic_avangala@quicinc.com>
2024-02-29 00:38:31 +0530

    asoc: pineapple: Fix dai link structure of btfm dai links
    Update dai link structure properly for btfm dai links.
    
    Change-Id: Ic82ba458d92c94cd668d1f8f0e4431eae31f7d5a
    Signed-off-by: Vangala, Amarnath <quic_avangala@quicinc.com>

--- asoc/pineapple.c
+++ asoc/pineapple.c
@@ -633,34 +633,12 @@ static struct snd_soc_dai_link msm_wcn_be_dai_links[] = {
 
 static struct snd_soc_dai_link msm_wcn_btfm_be_dai_links[] = {
 	{
-		.name = LPASS_BE_SLIMBUS_7_RX,
-		.stream_name = LPASS_BE_SLIMBUS_7_RX,
-		.playback_only = 1,
-		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
-			SND_SOC_DPCM_TRIGGER_POST},
-		.init = &msm_wcn_init,
-		.ops = &msm_common_be_ops,
-		/* dai link has playback support */
-		.ignore_pmdown_time = 1,
-		.ignore_suspend = 1,
-		SND_SOC_DAILINK_REG(slimbus_7_rx),
-	},
-	{
-		.name = LPASS_BE_SLIMBUS_7_TX,
-		.stream_name = LPASS_BE_SLIMBUS_7_TX,
-		.capture_only = 1,
-		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
-			SND_SOC_DPCM_TRIGGER_POST},
-		.ops = &msm_common_be_ops,
-		.ignore_suspend = 1,
-		SND_SOC_DAILINK_REG(slimbus_7_tx),
-	},
-	{
 		.name = LPASS_BE_SLIMBUS_8_TX,
 		.stream_name = LPASS_BE_SLIMBUS_8_TX,
 		.capture_only = 1,
 		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
 			SND_SOC_DPCM_TRIGGER_POST},
+		.init = &msm_wcn_init,
 		.ops = &msm_common_be_ops,
 		.ignore_suspend = 1,
 		SND_SOC_DAILINK_REG(slimbus_8_tx),
@@ -1328,7 +1306,10 @@ static struct snd_soc_dai_link msm_pineapple_dai_links[
 			ARRAY_SIZE(msm_va_cdc_dma_be_dai_links) +
 			ARRAY_SIZE(ext_disp_be_dai_link) +
 			ARRAY_SIZE(msm_common_be_dai_links) +
+#ifndef CONFIG_AUDIO_BTFM_PROXY
 			ARRAY_SIZE(msm_wcn_btfm_be_dai_links) +
+#endif
+			ARRAY_SIZE(msm_wcn_be_dai_links) +
 			ARRAY_SIZE(msm_mi2s_dai_links) +
 			ARRAY_SIZE(msm_tdm_dai_links)];
 
@@ -1672,6 +1653,7 @@ static struct snd_soc_card *populate_snd_card_dailinks(struct device *dev, int w
 			       msm_wcn_be_dai_links,
 			       sizeof(msm_wcn_be_dai_links));
 			total_links += ARRAY_SIZE(msm_wcn_be_dai_links);
+#ifndef CONFIG_AUDIO_BTFM_PROXY
 		} else {
 			rc = of_property_read_u32(dev->of_node, "qcom,wcn-btfm", &val);
 			if (!rc && val) {
@@ -1682,6 +1664,7 @@ static struct snd_soc_card *populate_snd_card_dailinks(struct device *dev, int w
 				       sizeof(msm_wcn_btfm_be_dai_links));
 				total_links += ARRAY_SIZE(msm_wcn_btfm_be_dai_links);
 			}
+#endif
 		}
 
 		dailink = msm_pineapple_dai_links;
