297691a0a932209ea96709126be75633d3376658
Maitreya25 <maitreyapatni25@gmail.com>
2024-06-08 16:59:42 +0530

    synaptics_hbp: Assign unique keycodes to every gesture
    Based on Luk1337's original work for touch v2
    
    Co-authored-by: LuK1337 <priv.luk@gmail.com>

--- oplus/kernel/touchpanel/synaptics_hbp/syna_tcm2.c
+++ oplus/kernel/touchpanel/synaptics_hbp/syna_tcm2.c
@@ -836,9 +836,9 @@ static void syna_dev_report_input_events(struct syna_tcm *tcm)
 					touchpanel_event_call_notifier(EVENT_ACTION_UNDER_WATER, (void *)&tcm->under_water);
 				}
 			} else {
-				input_report_key(input_dev, KEY_F4, 1);
+				input_report_key(input_dev, 246 + touch_data->g_type, 1); 
 				input_sync(input_dev);
-				input_report_key(input_dev, KEY_F4, 0);
+				input_report_key(input_dev, 246 + touch_data->g_type, 0);
 				input_sync(input_dev);
 			}
 		}
@@ -949,7 +949,7 @@ exit:
  */
 static int syna_dev_create_input_device(struct syna_tcm *tcm)
 {
-	int retval = 0;
+	int retval = 0, i = 0;
 	struct tcm_dev *tcm_dev = tcm->tcm_dev;
 	struct input_dev *input_dev = NULL;
 
@@ -1006,6 +1006,9 @@ static int syna_dev_create_input_device(struct syna_tcm *tcm)
 	input_set_capability(input_dev, EV_KEY, KEY_UNDER_WATER);
 	set_bit(KEY_ON_WATER, input_dev->keybit);
 	input_set_capability(input_dev, EV_KEY, KEY_ON_WATER);
+	for (i = UP_VEE; i <= S_GESTURE; i++) {
+		set_bit(246 + i, input_dev->keybit);
+	}
 #endif
 
 	input_set_abs_params(input_dev,
