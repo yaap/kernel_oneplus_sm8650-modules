76502a3e5d131d6153b66f904d6bb789db9d966c
Kazuki Hashimoto <kazukih0205@gmail.com>
2024-12-17 15:21:11 +0000

    eva-kernel: cvp: Don't use PM QoS

--- qcom/opensource/eva-kernel/msm/eva/cvp_hfi.c
+++ qcom/opensource/eva-kernel/msm/eva/cvp_hfi.c
@@ -2150,37 +2150,8 @@ static int __sys_set_power_control(struct iris_hfi_device *device,
 
 static void cvp_pm_qos_update(struct iris_hfi_device *device, bool vote_on)
 {
-	u32 latency, off_vote_cnt;
-	int i, err = 0;
-
-	spin_lock(&device->res->pm_qos.lock);
-	off_vote_cnt = device->res->pm_qos.off_vote_cnt;
-	spin_unlock(&device->res->pm_qos.lock);
-
-	if (vote_on && off_vote_cnt)
-		return;
-
-	latency = vote_on ? device->res->pm_qos.latency_us :
-			PM_QOS_RESUME_LATENCY_DEFAULT_VALUE;
-
-	if (device->res->pm_qos.latency_us && device->res->pm_qos.pm_qos_hdls)
-		for (i = 0; i < device->res->pm_qos.silver_count; i++) {
-			if (!cpu_possible(device->res->pm_qos.silver_cores[i]))
-				continue;
-			err = dev_pm_qos_update_request(
-				&device->res->pm_qos.pm_qos_hdls[i],
-				latency);
-			if (err < 0) {
-				if (vote_on) {
-					dprintk(CVP_WARN,
-						"pm qos on failed %d\n", err);
-				} else {
-					dprintk(CVP_WARN,
-						"pm qos off failed %d\n", err);
-				}
-			}
-		}
 }
+
 static int iris_pm_qos_update(void *device)
 {
 	struct iris_hfi_device *dev;
