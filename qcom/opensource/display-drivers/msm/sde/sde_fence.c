65d7ce3fef422c38fef149bec831b64a34101d31
Park Ju Hyung <qkrwngud825@gmail.com>
2025-01-18 11:40:02 +0000

    display-drivers: msm: sde: Use kmem_cache pool for struct sde_fence
    These get allocated and freed millions of times on this kernel tree.
    
    Use a dedicated kmem_cache pool and avoid costly dynamic memory allocations.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- qcom/opensource/display-drivers/msm/sde/sde_fence.c
+++ qcom/opensource/display-drivers/msm/sde/sde_fence.c
@@ -17,6 +17,8 @@
 #include "sde_trace.h"
 //#endif /*OPLUS_ARCH_EXTENDS*/
 
+static struct kmem_cache *kmem_fence_pool;
+
 #define TIMELINE_VAL_LENGTH		128
 #define SPEC_FENCE_FLAG_FENCE_ARRAY	0x10
 #define SPEC_FENCE_FLAG_ARRAY_BIND	0x11
@@ -904,7 +906,7 @@ static void sde_fence_release(struct dma_fence *fence)
 			_sde_hw_fence_release(f);
 
 		kref_put(&f->ctx->kref, sde_fence_destroy);
-		kfree(f);
+		kmem_cache_free(kmem_fence_pool, f);
 	}
 }
 
@@ -957,7 +959,7 @@ static int _sde_fence_create_fd(void *fence_ctx, uint32_t val, struct sde_hw_ctl
 		goto exit;
 	}
 
-	sde_fence = kzalloc(sizeof(*sde_fence), GFP_KERNEL);
+	sde_fence = kmem_cache_zalloc(kmem_fence_pool, GFP_KERNEL);
 	if (!sde_fence)
 		return -ENOMEM;
 
@@ -1019,6 +1021,8 @@ struct sde_fence_context *sde_fence_init(const char *name, uint32_t drm_id)
 		return ERR_PTR(-ENOMEM);
 	}
 
+	kmem_fence_pool = KMEM_CACHE(sde_fence, SLAB_HWCACHE_ALIGN | SLAB_PANIC);
+
 	strlcpy(ctx->name, name, ARRAY_SIZE(ctx->name));
 	ctx->drm_id = drm_id;
 	kref_init(&ctx->kref);
@@ -1039,6 +1043,8 @@ void sde_fence_deinit(struct sde_fence_context *ctx)
 	}
 
 	kref_put(&ctx->kref, sde_fence_destroy);
+
+	kmem_cache_destroy(kmem_fence_pool);
 }
 
 void sde_fence_prepare(struct sde_fence_context *ctx)
